81ee55dfcca0444045537b0fb436aaf4991ccb64
keegang6705 <75747783+keegang6705@users.noreply.github.com>
2023-05-08 22:29:21 +0700

    Update autotype.py

--- autotype.py
+++ autotype.py
@@ -1,11 +1,16 @@
 import pyautogui 
 import time
+#var
+path = r"C:\\path_to_text_file\\text.txt"
+delay_before_start = 3
+type_interval = 0.05
+
 print("open file...")
-txt = open(r"C:\\path_to_text_file\\text.txt", "r")
+txt = open(path, "r")
 print("prepare for autotyping...")
-time.sleep(5)
+time.sleep(delay_before_start)
 print("start typing...")
 for c in txt:
-   pyautogui.typewrite(c, interval=0.05)
+   pyautogui.typewrite(c, interval=type_interval)
    pyautogui.press("enter")
 print("end typing")
